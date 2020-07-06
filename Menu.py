bb1fa727f10e59553edca4dee5d3f900670b53ee
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-07-06 14:58:35 -0600

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -25,11 +25,22 @@ while True:
 	for event in pygame.event.get():
 		 if event.type == pygame.QUIT:
 		 	sys.exit()
+		 if event.type == pygame.MOUSEBUTTONDOWN:
+		 	mouse = event.pos
+		 	print(mouse)
+		 	if mouse[0]>270 and mouse[0]<470 and mouse[1]>270 and mouse [1]<370: #boton jugar
+		 		print("jugar")
+		 	if mouse[0]>250 and mouse[0]<485 and mouse[1]>420 and mouse [1]<525: #boton ajustes 
+		 		print("ajustes")
+		 	if mouse[0]>635 and mouse[0]<741 and mouse[1]>695 and mouse [1]<775: #boton ayuda
+		 		print("ayuda")
+		 	if mouse[0]>26 and mouse[0]<295 and mouse[1]>705 and mouse [1]<790: #boton creditos 
+		 		print("creditos")
 	#Fondo
 	screen.blit(background,[0,0])
 	#Botones
-	screen.blit(jugar, [250,200])
-	screen.blit(ajustes, [230,350])
+	screen.blit(jugar, [250,250])
+	screen.blit(ajustes, [230,400])
 	screen.blit(ayuda, [620,680])
 	screen.blit(creditos, [10,680])
 	#Titulo
