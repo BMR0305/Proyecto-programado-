15f52a9ec9d42057a9ad767496babc4650ae46b4
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-07-08 17:25:31 -0600

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -30,15 +30,20 @@ color_inactive = pygame.Color(255,255,255)
 color_active = pygame.Color(155,155,155)
 color = color_inactive
 input_box = pygame.Rect(320, 185, 230, 40)
-while True:
+game_over = True
+while game_over:
 
 	for event in pygame.event.get():
 		 if event.type == pygame.QUIT:
+		 	game_over = False 
 		 	sys.exit()
 		 if event.type == pygame.MOUSEBUTTONDOWN: 
 		 	mouse = event.pos
 		 	if mouse[0]>270 and mouse[0]<470 and mouse[1]>270 and mouse [1]<370: #boton jugar
+		 		game_over= False
+		 		pygame.quit()
 		 		os.system("Matriz.py")
+
 		 	if mouse[0]>250 and mouse[0]<485 and mouse[1]>420 and mouse [1]<525: #boton ajustes 
 		 		print("ajustes")
 		 	if mouse[0]>635 and mouse[0]<741 and mouse[1]>695 and mouse [1]<775: #boton ayuda
@@ -68,23 +73,28 @@ while True:
 		 			text += event.unicode
 
 
+	try:
+		#Fondo
+		screen.blit(background,[0,0])
+		#Botones
+		screen.blit(jugar, [250,250])
+		screen.blit(ajustes, [230,400])
+		screen.blit(ayuda, [620,680])
+		screen.blit(creditos, [10,680])
+		screen.blit(usuario,[175,175])
+		#Titulo
+		screen.blit(titulo, [90,10])
+		#Input box
+		pygame.draw.rect(screen, color, input_box, 4)
+		#Renderiza el texto
+		txt_surface = font.render(text, True, color_inactive)
+		screen.blit(txt_surface, (input_box.x+5, input_box.y+7))
+		
+		pygame.display.flip()
+		clock.tick(60)
+	except:
+		pass
 
-	#Fondo
-	screen.blit(background,[0,0])
-	#Botones
-	screen.blit(jugar, [250,250])
-	screen.blit(ajustes, [230,400])
-	screen.blit(ayuda, [620,680])
-	screen.blit(creditos, [10,680])
-	screen.blit(usuario,[175,175])
-	#Titulo
-	screen.blit(titulo, [90,10])
-	#Input box
-	pygame.draw.rect(screen, color, input_box, 4)
-	#Renderiza el texto
-	txt_surface = font.render(text, True, color_inactive)
-	screen.blit(txt_surface, (input_box.x+5, input_box.y+7))
 	
 
-	pygame.display.flip()
-	clock.tick(60)
+sys.exit()
