f185eff7958bc60084b3d5310b51e917fb1619e3
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-07-15 10:50:50 -0600

    Update herencia i-2020.py

--- herencia i-2020.py
+++ herencia i-2020.py
@@ -50,7 +50,7 @@ class Moto(Vehiculo):
         print("------------------------------")
 
 vehiculos = [Auto("1234", "BMW", 4, 11000, "Gasolina", 500, "Serie 2", 2017), Auto("1334", "BMW", 4, 1000, "Gasolina", 700, "R8", 2020), Auto("1256", "Toyota", 4, 15000, "Gasolina", 400, "Corolla", 2018), Auto("7834", "Toyota", 4, 6000, "Diesel", 800, "Hilux", 2020),
-             Moto("5876", "Kawasiki", 2, 20000, "Gasolina", 400, "Z1000", 1.043), Moto("1242", "Kawasaki", 2, 12000, "Gasolina", 600, "KX250", 249), Moto("1487", "Honda", 2, 12000, "Gasolina", 300, "CB 190 R", 184.40), Moto("5292", "Suzuki", 2, 18000, "Gasolina", 350, "EN-125 2A", 125)] 
+             Moto("5876", "Kawasaki", 2, 20000, "Gasolina", 400, "Z1000", 1.043), Moto("1242", "Kawasaki", 2, 12000, "Gasolina", 600, "KX250", 249), Moto("1487", "Honda", 2, 12000, "Gasolina", 300, "CB 190 R", 184.40), Moto("5292", "Suzuki", 2, 18000, "Gasolina", 350, "EN-125 2A", 125)] 
 
 
 def revise(lista):
